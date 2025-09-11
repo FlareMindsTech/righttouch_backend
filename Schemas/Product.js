334803b8c4c30c7e3541178d88efad9afc638a4c
V1GNESH-SV <vigneshubi24@gmail.com>
2025-09-11 11:36:55 +0530

    Write a product Schema

---
+++ Schemas/Product.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const Product = mongoose.Schema({
+  productName: {
+    type: String,
+    require: true,
+  },
+  productDescription: {
+    type: String,
+    require: true,
+  },
+  productPrice: {
+    type: Number,
+    require: true,
+  },
+  productDiscountPrice: {
+    type: Number,
+    require: true,
+  },
+  productImage: {
+    type: [String],
+    default: [],
+  },
+  productBrand: {
+    type: String,
+    require: true,
+  },
+  productFeatures: {
+    type: [String],
+    default:[]
+  },
+  status: {
+    type: String,
+    enum: ["Available", "Unavailable"],
+    default: "Available"
+  },
+  warranty: { type: String },
+  createdAt: { type: Date, default: Date.now },
+});
