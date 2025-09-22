230b564257af63302c792a4ee9ae5d03a8072d74
Gokila-M <gokila1305@gmail.com>
2025-09-22 21:41:26 +0530

    ../ commit

--- index.js
+++ index.js
@@ -18,20 +18,20 @@ App.use(bodyParser.urlencoded({ extended: true }));
 App.use(express.static("public"));
 
 mongoose.set("strictQuery", false);
-mongoose.connect("mongodb://127.0.0.1:27017/Righttouchdatabase")
-  .then(() => console.log("You! Connected to MongoDB..."))
-  .catch((err) =>
-    console.error("Could not connect to MongoDB... " + err.message)
-  );
-// mongoose.connect('mongodb+srv://flaremindstech:flareminds%401308@cluster0.12wutsc.mongodb.net/emailsender?retryWrites=true&w=majority&appName=Cluster0')
-// .then(() => console.log('Connected to MongoDB Atlas...'))
-// .catch(err => console.error('Could not connect to MongoDB...'));
+// mongoose.connect("mongodb://127.0.0.1:27017/Righttouchdatabase")
+//   .then(() => console.log("You! Connected to MongoDB..."))
+//   .catch((err) =>
+//     console.error("Could not connect to MongoDB... " + err.message)
+//   );
+mongoose.connect('mongodb+srv://flaremindstech:flareminds%401308@cluster0.12wutsc.mongodb.net/emailsender?retryWrites=true&w=majority&appName=Cluster0')
+.then(() => console.log('Connected to MongoDB Atlas...'))
+.catch(err => console.error('Could not connect to MongoDB...'));
  
 App.get("/", (req, res) => {
   res.send("welcome"); 
 });
 
-
+ 
 // Routes
 App.use('/api/user', UserRoutes);
 App.use('/api/technician', TechnicianRoutes);
